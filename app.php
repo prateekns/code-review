1a4158f6d4c02ddd275db82df6857edb53da99c5
Prateek Tyagi <prateek.tyagi@netsolutions.com>
2026-04-14 11:27:42 +0530

    Test changes

--- app.php
+++ app.php
@@ -5,8 +5,6 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-echo "Script started...\n";
-
 /**
  * AI PR Review POC - single file, no dependencies.
  *
